8389fdb1b953e7d13d27c6397bb0271f2ec3b1c2
Elaine Victoria <111344174+walts2@users.noreply.github.com>
2023-06-19 00:48:41 +0800

    Add files via upload

---
+++ LibraryFolder/BookOne.java
@@ -0,0 +1,32 @@
+
+public class BookOne {
+	private String title;
+    private String author;
+    private int yearPublished;
+    private int numCopies;
+    
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public int getYearPublished() {
+		return yearPublished;
+	}
+	public void setYearPublished(int yearPublished) {
+		this.yearPublished = yearPublished;
+	}
+	public int getNumCopies() {
+		return numCopies;
+	}
+	public void setNumCopies(int numCopies) {
+		this.numCopies = numCopies;
+	}
+}
