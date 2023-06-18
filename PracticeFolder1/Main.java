3095858e1bd234ea417b141d6d8bff0933b3b7ec
Elaine Victoria <111344174+walts2@users.noreply.github.com>
2023-06-18 23:15:47 +0800

    Create Main.java

---
+++ PracticeFolder1/Main.java
@@ -0,0 +1,5 @@
+TxtMembers m = new TxtMembers("Soobin", "Choi", "Leader" , 22);
+		TxtMembers m1 = new TxtMembers("Yeonjun", "Choi", "Main Dancer", 23);
+		
+		System.out.println(m.firstname);
+		System.out.println(m1.firstname);
