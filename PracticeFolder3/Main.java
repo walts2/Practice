cc8866d91e813f4b4052c49169655d8e247d1e6b
Elaine Victoria <111344174+walts2@users.noreply.github.com>
2023-06-19 00:20:20 +0800

    Create Main.java

---
+++ PracticeFolder3/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Student s = new Student("Walt", "Siegrain", "3", "BS-IT", "D", 88.5f, 85.5f);
+		s.IntroduceSelf();
+		s.EvaluateGrade();
+	}
+}
