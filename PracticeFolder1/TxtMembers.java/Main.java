bb88a2437b5e9d93d91f647bee4d6bd3e8ce4222
Elaine Victoria <111344174+walts2@users.noreply.github.com>
2023-06-18 23:14:14 +0800

    Create Main.java

---
+++ PracticeFolder1/TxtMembers.java/Main.java
@@ -0,0 +1,15 @@
+
+public class TxtMembers {
+	
+	String firstname;
+	String lastname;
+	String position;
+	int age;
+	
+	TxtMembers(String firstname, String lastname, String position, int age){
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.position = position;
+		this.age = age;
+	}	
+}
