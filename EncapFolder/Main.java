67dc7420087f8c7997912ab6bb493ff244a6d322
Elaine Victoria <111344174+walts2@users.noreply.github.com>
2023-06-19 02:30:36 +0800

    Create Main.java

---
+++ EncapFolder/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		User u = new User(01234, "Walt", "Walt", "Siege");
+		//u.setUserID(1234);
+		
+		String firstname = u.getfirstname();
+		System.out.println(firstname);
+		
+	}
+}
